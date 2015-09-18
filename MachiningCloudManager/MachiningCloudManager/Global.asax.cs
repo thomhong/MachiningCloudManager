0f3550958986b81feb2ddb5b7002b5e0734ca76f
Thomas Hong <thomas.hong@machiningcloud.com>
2015-09-18 13:40:16 -0700

    Added print statement #MCM-1

--- MachiningCloudManager/MachiningCloudManager/Global.asax.cs
+++ MachiningCloudManager/MachiningCloudManager/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -15,6 +16,9 @@ namespace MachiningCloudManager
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            Console.Write("ggg");
         }
     }
 }
